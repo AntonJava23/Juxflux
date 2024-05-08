f9b7ca1a5549382bd413c3f8cf581b2c12ebcca8
demi-byte <guttesolstralar@gmail.com>
2024-05-08 11:30:39 +0200

    merging

---
+++ js/app-branch2.js
@@ -0,0 +1,10 @@
+import QuestionTwo from "./components/QuestionTwo.js";
+
+const app = {  
+    
+    
+}
+
+const vueApp = Vue.createApp(app)
+vueApp.component('question-two', QuestionTwo)
+vueApp.mount("#app")
