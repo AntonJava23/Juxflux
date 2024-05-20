dde36a30dd3ef46cbde09dcc5f0f68c27e4fcde1
demi-byte <guttesolstralar@gmail.com>
2024-05-20 09:45:58 +0200

    fixing game over component

--- js/components/gameover.js
+++ js/components/gameover.js
@@ -8,13 +8,13 @@ export default{
     },
 
     created() {
-        store.score = 0
-        sessionStorage.setItem('score', store.score)
     },
     methods: {
         redirect(){
             store.score = 0
+            store.health = 3
             sessionStorage.setItem('score', store.score)
+            sessionStorage.setItem('health', store.health)
             window.location.href = "index-branch2.html"
         }
     },
@@ -23,10 +23,9 @@ export default{
        <h1>Game Over</h1>
       <p id="point-store">
 
-    <img id="firework" src="images\Fireworks.png">
+    <img id="firework" src="images/Fireworks.png">
       <p> {{store.score}} </p>
-      <img src="images\riksdagen.png">
+      <img src="images/riksdagen.png">
       <button @click="redirect" id="restart-button">Spela Igen</button>
-      <game-over></game-over>
     </div>`
 }
