e3efd693183d2565cbb290248e930afa9c14b25d
Gurdhi0115 <150341211+Gurdhi0115@users.noreply.github.com>
2024-05-20 09:37:27 +0200

    jsdbjs

--- js/GameOver.js
+++ js/GameOver.js
@@ -1,7 +1,5 @@
 
-
-
-  const startMenuText = document.getElementById('start-menu-text');
+const startMenuText = document.getElementById('start-menu-text');
 document.getElementById("start-menu-text").addEventListener("click", function () {
   window.location.href = "StartmenyVue.html"
 });
