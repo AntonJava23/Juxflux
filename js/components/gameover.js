799c570f989824bbf5eaa232492fb3ff1249aa1e
ThereseFrimanson <thefri0827@skola.goteborg.se>
2024-05-20 11:38:17 +0200

    small changes on gameover css

--- js/components/gameover.js
+++ js/components/gameover.js
@@ -30,7 +30,7 @@ export default{
 
     <img id="firework" src="images/Fireworks.png">
       <p> {{store.score}} </p>
-      <img @click="homeImage" id="homeImage" src="images/riksdagen.png">
+      <img @click="homeImage" id="homeImage" src="images/riksdagenBig.png">
       <span> Hem </span>
       <button @click="redirect" id="restart-button">Spela Igen</button>
     </div>`
