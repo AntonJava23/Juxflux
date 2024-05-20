2ba2db2a68adc769577f1bcc2d0982abce43e91d
AntonJava23 <antbri1009@skola.goteborg.se>
2024-05-20 08:18:55 +0200

    Some work on Gameover files

---
+++ js/util/GameOver.js
@@ -0,0 +1,10 @@
+const restartButton = document.getElementById('restart-button');
+document.getElementById("restart-button").addEventListener('click', function() {
+    // Reset the game and create a new game object
+    window.location.href = "index-branch2.html" 
+  });
+
+  const startmenutext = document.getElementById('start-menu-text');
+    document.getElementById("start-menu-text").addEventListener('click', function(){
+      window.location.href = "Startmeny.html"
+    });
