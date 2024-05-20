dfdab350746b5180e6fdaeb01f5f579b1132dfdb
Gurdhi0115 <150341211+Gurdhi0115@users.noreply.github.com>
2024-05-20 11:47:38 +0200

    update

--- js/components/gameover.js
+++ js/components/gameover.js
@@ -27,9 +27,7 @@ export default{
     <div id="app">
        <h1>Game Over</h1>
       <p id="point-store"></p>
-
-    <img id="firework" src="images/Fireworks.png">
-      <p> {{store.score}} </p>
+      <p id="points"> {{store.score}} </p>
       <img @click="homeImage" id="homeImage" src="images/riksdagen.png">
       <span> Hem </span>
       <button @click="redirect" id="restart-button">Spela Igen</button>
