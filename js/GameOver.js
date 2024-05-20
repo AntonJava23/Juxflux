0cc987332687763cb8e5cad738486618dfada887
Gurdhi0115 <150341211+Gurdhi0115@users.noreply.github.com>
2024-05-20 09:27:50 +0200

    update1

--- js/GameOver.js
+++ js/GameOver.js
@@ -1,21 +1,5 @@
 
-import { store } from "./store/StoreData";
 
-
-function showPointStore() {
-
-  const pointStoreElement = document.getElementById('point-store');
-  pointStoreElement.innerHTML = `
-  //<p>Pöang: ${store.points}</p>  `;
-
-  }
-
-  showPointStore();
-
-  const restartButton = document.getElementById('restart-button');
-  document.getElementById("restart-button").addEventListener("click", function () {
-    window.location.href = "index-branch2.html"
-  });
 
   const startMenuText = document.getElementById('start-menu-text');
 document.getElementById("start-menu-text").addEventListener("click", function () {
